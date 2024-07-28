76db10a9abc71af5915c2db848698c6510fc13f4
TimeTravelerFromNow <sebastian.detering@gmail.com>
2024-07-28 13:04:45 -0700

    dropdown blog javascript

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,7 @@ pin "bootstrap" # @5.3.2
 pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.js"
+pin "@floating-ui/core", to: "@floating-ui--core.js" # @1.6.5
+pin "@floating-ui/utils", to: "@floating-ui--utils.js" # @0.2.5
+pin "@floating-ui/dom", to: "@floating-ui--dom.js" # @1.6.8
+pin "@floating-ui/utils/dom", to: "@floating-ui--utils--dom.js" # @0.2.5

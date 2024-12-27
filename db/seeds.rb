8abe6c36539e8ebc060eabae5a9214da2a877f77
Sebastian Detering <98575184+TimeTravelerFromNow@users.noreply.github.com>
2024-12-26 18:10:48 -0800

    Add images to database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,20 +8,36 @@
 
 Blog.destroy_all
 
+
 Blog.create!([
   {
   title: "Rails Developers Unite",
   description: "latest development blog",
-  address: "dev-updates"
-},
+  address: "rails-dev-blog"
+  },
   {
   title: "Gaming",
   description: "my journey playing all sorts of games",
   address: "gaming-blog"
-},
+  },
   {
   title: "Travel Blog",
   description: "Very first travel blog",
-  address: "arizona-travels"
-}
+  address: "travel-blog"
+  }
 ])
+
+# Add images as active storage file attachments
+b0 = Blog.find_by(address: 'rails-dev-blog')
+f0 = File.open( Rails.root.join('db/images/rails-development-window.jpg') )
+b0.header_image.attach(io: f0, filename: 'rails-development-window.jpg')
+
+b1 = Blog.find_by(address: 'gaming-blog')
+f1 = File.open(  Rails.root.join('db/images/videogame-controller.jpg') )
+b1.header_image.attach(io: f1, filename: 'videogame-controller.jpg')
+
+b2 = Blog.find_by(address: 'travel-blog')
+f2 = File.open( Rails.root.join('db/images/arizona-flag.jpg') )
+b2.header_image.attach(io: f2, filename: 'arizona-flag.jpg')
+
+
